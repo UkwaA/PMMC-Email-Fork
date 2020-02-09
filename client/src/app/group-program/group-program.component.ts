81b67fb3499320016acfcdc97124646fb2210e57
anhnn5 <anhnn5@uci.edu>
2020-02-08 22:04:01 -0800

    anhnn5-Added content for two Program Page

--- client/src/app/group-program/group-program.component.ts
+++ client/src/app/group-program/group-program.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProgramData} from '../data/program-data';
 
 @Component({
     templateUrl: './group-program.component.html',
@@ -6,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class GroupProgramComponent implements OnInit{
+    programs:ProgramData[];
     constructor(){
 
     }
+    
     ngOnInit(){
+        this.programs = this.createPrograms();
     }
+
+    createPrograms():ProgramData[]{
+        let  i;
+        let result = [];
+        for (i = 1; i < 7; ++i){
+            let program = new ProgramData('Title'.concat(i.toString()), 'This is the Description of '.concat(i.toString()), "https://d.newsweek.com/en/full/615399/grey-seals.jpg", "https://www.pacificmmc.org/");
+            result.push(program);
+        }
+        return result;
+    }
+
 }
--- client/src/app/individual-program/individual-program.component.ts
+++ client/src/app/individual-program/individual-program.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProgramData} from '../data/program-data';
 
 @Component({
     templateUrl: './individual-program.component.html',
@@ -6,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class IndividualProgramComponent implements OnInit{
+    programs:ProgramData[];
     constructor(){
 
     }
     ngOnInit(){
+        this.programs = this.createPrograms();
+    }
+
+    createPrograms():ProgramData[]{
+        let  i;
+        let result = [];
+        for (i = 1; i < 9; ++i){
+            let program = new ProgramData('Title'.concat(i.toString()), 'This is the Description of '.concat(i.toString()), "https://d.newsweek.com/en/full/615399/grey-seals.jpg", "https://www.pacificmmc.org/");
+            result.push(program);
+        }
+        return result;
     }
 }
