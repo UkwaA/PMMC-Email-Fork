899a8158189a2ae177e2af838c1317a3099ce128
anhnn5 <anhnn5@uci.edu>
2020-02-17 10:16:39 -0800

    anhnn5-load data from database to program cards for homepage, group program page and individual program page.

--- client/src/app/group-program/group-program.component.ts
+++ client/src/app/group-program/group-program.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProgramData} from '../data/program-data';
+import { ProgramServices } from '../services/program.services';
 
 @Component({
     templateUrl: './group-program.component.html',
@@ -8,22 +9,13 @@ import {ProgramData} from '../data/program-data';
 
 export class GroupProgramComponent implements OnInit{
     programs:ProgramData[];
-    constructor(){
+    constructor(private programService: ProgramServices){
 
     }
-    
     ngOnInit(){
-        this.programs = this.createPrograms();
-    }
-
-    createPrograms():ProgramData[]{
-        let  i;
-        let result = [];
-        for (i = 1; i < 7; ++i){
-            let program = new ProgramData('Title'.concat(i.toString()), 'This is the Description of '.concat(i.toString()), "https://d.newsweek.com/en/full/615399/grey-seals.jpg", "https://www.pacificmmc.org/", 200);
-            result.push(program);
-        }
-        return result;
+        this.programService.getAllPrograms().then((result) =>{
+            this.programs = result;
+        })
     }
 
 }
--- client/src/app/individual-program/individual-program.component.ts
+++ client/src/app/individual-program/individual-program.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProgramData} from '../data/program-data';
+import { ProgramServices } from '../services/program.services';
 
 @Component({
     templateUrl: './individual-program.component.html',
@@ -8,20 +9,12 @@ import {ProgramData} from '../data/program-data';
 
 export class IndividualProgramComponent implements OnInit{
     programs:ProgramData[];
-    constructor(){
+    constructor(private programService: ProgramServices){
 
     }
     ngOnInit(){
-        this.programs = this.createPrograms();
-    }
-
-    createPrograms():ProgramData[]{
-        let  i;
-        let result = [];
-        for (i = 1; i < 9; ++i){
-            let program = new ProgramData('Title'.concat(i.toString()), 'This is the Description of '.concat(i.toString()), "https://d.newsweek.com/en/full/615399/grey-seals.jpg", "https://www.pacificmmc.org/", 300);
-            result.push(program);
-        }
-        return result;
+        this.programService.getAllPrograms().then((result) =>{
+            this.programs = result;
+        })
     }
 }
