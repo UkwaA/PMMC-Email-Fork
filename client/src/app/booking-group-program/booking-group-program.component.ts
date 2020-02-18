b7da9afcc2a4bf3ea99cc6c7bbcbc7fbeac24b8e
Nha Vuong <46470817+nhavuong@users.noreply.github.com>
2020-02-17 23:07:18 -0800

    nhatv-add more Validation

--- client/src/app/booking-group-program/booking-group-program.component.ts
+++ client/src/app/booking-group-program/booking-group-program.component.ts
@@ -17,23 +17,23 @@ export class BookingGroupProgramComponent implements OnInit {
   ngOnInit() {
     this.bookingGroup = new BookingGroupData(true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true);
     this.registerForm = this.fb.group({
-      AldultQuantity: ['', Validators.required],
-      Age57Quantity: ['', Validators.required],
-      Age810Quantity: ['', Validators.required],
-      Age1113Quantity: ['', Validators.required],
+      AldultQuantity: ['', [Validators.required, Validators.max(35)]],
+      Age57Quantity: ['', [Validators.required, Validators.max(35)]],
+      Age810Quantity: ['', [Validators.required, Validators.max(35)]],
+      Age1113Quantity: ['', [Validators.required, Validators.max(35)]],
       ScoutProgram: ['', Validators.required],
       EducationFK: ['', Validators.required],
-      OrganizationName: ['', Validators.required],
-      TeacherName: ['', Validators.required],
+      OrganizationName: ['', [Validators.required, Validators.minLength(3)]],
+      TeacherName: ['', [Validators.required, Validators.minLength(6)]],
       TeacherEmail: ['', [Validators.required, Validators.email]],
-      TeacherPhoneNo: ['', Validators.required],
+      TeacherPhoneNo: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'), Validators.minLength(10)]],
       GradeLevel: ['', Validators.required],
       TotalQuantity: ['', Validators.required],
       ProgramRestriction: ['', Validators.required]
 
     });
   }
-  
+
   get f() { return this.registerForm.controls;}
 
   onSubmit() {
