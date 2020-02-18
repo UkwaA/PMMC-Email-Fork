63139996f3a9fcaf91f611ad38f9b268329ecb17
nhathuy007 <55850287+nhathuy007@users.noreply.github.com>
2020-02-18 13:08:06 -0800

    hnluong1-add dotenv file

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
+require('dotenv').config()
 const Sequelize = require('sequelize')
 const db = {}
 
-process.env.DBNAME = 'pmmc'
-process.env.USERNAME = 'root'
-process.env.PASSWORD = '123456'
-
 
 // Connection to LocalDB
-const sequelize = new Sequelize(process.env.DBNAME, process.env.USERNAME, process.env.PASSWORD, {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   define: {
     timestamps: false
