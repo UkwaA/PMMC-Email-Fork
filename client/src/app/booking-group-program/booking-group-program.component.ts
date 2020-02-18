623d095f72a0d3e25b3d6e669150ff272f7863ee
Nha Vuong <46470817+nhavuong@users.noreply.github.com>
2020-02-17 22:17:42 -0800

    nhatv- Validation for Group program

--- client/src/app/booking-group-program/booking-group-program.component.ts
+++ client/src/app/booking-group-program/booking-group-program.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookingGroupData } from '../data/booking-group-data';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-booking-group-program',
@@ -8,10 +9,42 @@ import { BookingGroupData } from '../data/booking-group-data';
 })
 export class BookingGroupProgramComponent implements OnInit {
   bookingGroup: BookingGroupData;
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.bookingGroup = new BookingGroupData(true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true);
-  }
+    this.registerForm = this.fb.group({
+      AldultQuantity: ['', Validators.required],
+      Age57Quantity: ['', Validators.required],
+      Age810Quantity: ['', Validators.required],
+      Age1113Quantity: ['', Validators.required],
+      ScoutProgram: ['', Validators.required],
+      EducationFK: ['', Validators.required],
+      OrganizationName: ['', Validators.required],
+      TeacherName: ['', Validators.required],
+      TeacherEmail: ['', [Validators.required, Validators.email]],
+      TeacherPhoneNo: ['', Validators.required],
+      GradeLevel: ['', Validators.required],
+      TotalQuantity: ['', Validators.required],
+      ProgramRestriction: ['', Validators.required]
 
+    });
+  }
+  
+  get f() { return this.registerForm.controls;}
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+}
 }
