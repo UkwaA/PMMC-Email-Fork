c83ad3f68d607722f769088db4ec1faf9e84c3f8
Nha Vuong <46470817+nhavuong@users.noreply.github.com>
2020-02-18 10:57:27 -0800

    nhatv-validation subcribe email, animation social

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { AuthenticationService } from './authentication.service';
 import { faFacebook,faYoutube, faYelp} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faPhone, faMapMarkedAlt, faHandHoldingUsd, faDoorOpen, faLaughWink} from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  constructor(public auth: AuthenticationService) {}
+export class AppComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(public auth: AuthenticationService, private fb: FormBuilder) { }
+
   faFacebook = faFacebook;
   faYoutube = faYoutube;
   faYelp = faYelp;
@@ -18,5 +23,25 @@ export class AppComponent {
   faHandHoldingUsd = faHandHoldingUsd;
   faDoorOpen = faDoorOpen;
   faLaughWink = faLaughWink;
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      subscribeEmail: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  get f() { return this.registerForm.controls;}
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+}
 
 }
