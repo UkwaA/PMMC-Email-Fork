0b09dec0ecc63f74f39b17408fc6173769ccc633
anhnn5 <anhnn5@uci.edu>
2020-02-20 00:28:40 -0800

    anhnn5-fixed i-program components and added it to program detail page.

--- client/src/app/components/i-program/i-program.component.ts
+++ client/src/app/components/i-program/i-program.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProgramServices } from 'src/app/services/program.services';
 import { BookingIndividualData } from 'src/app/data/booking-individual-data';
+import { getTreeNoValidDataSourceError } from '@angular/cdk/tree';
 
 @Component({
   selector: 'i-program',
@@ -10,9 +11,15 @@ import { BookingIndividualData } from 'src/app/data/booking-individual-data';
 export class IProgramComponent implements OnInit {
 
   bookingIndividual: BookingIndividualData;
+ 
   //TODO: inject the service
   constructor(private service:ProgramServices) { }
 
   ngOnInit() {
+    this.bookingIndividual = new BookingIndividualData(false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,false);
+  }
+
+  getVar(varName:string):string{
+    return "bookingIndividual." + varName;
   }
 }
