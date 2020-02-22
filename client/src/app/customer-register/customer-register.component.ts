14075e57c46c169b534ae1c15630fff53fc329f3
UkwaA <uakkum@uci.edu>
2020-02-22 14:10:26 -0800

    -uakkum added confirmed password matching

--- client/src/app/customer-register/customer-register.component.ts
+++ client/src/app/customer-register/customer-register.component.ts
@@ -8,24 +8,37 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CustomerRegisterComponent implements OnInit {
   customerForm = new FormGroup({
-    firstName: new FormControl(''),
+    firstName: new FormControl('', Validators.required),
     middleInitials: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    phoneNum: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl('')});
-  constructor() { }
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email),
+    phoneNum: new FormControl('', Validators.minLength(9)),
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.minLength(6)),
+    confirmed_password: new FormControl('', Validators.minLength(6))
+  });
+  
+    constructor() { }
 
   ngOnInit() {
     console.log(this.customerForm.value);
   }
 
-  toggleDisabled(): any {
+  checkSubmission(): any {
     console.log("This button works");
     console.log(this.customerForm.value);
+    return this.customerForm.status;
     // let submitButton = <HTMLInputElement> document.getElementById('submit_btn');
     // submitButton.disabled = !submitButton.disabled;
     // console.warn(testButton.disabled);
+  }
+
+  passwordsMatch(): any{
+    console.log("Checking passowrds");
+    if(this.customerForm.get('password').value!= '' && this.customerForm.get('confirmed_password').value!= ''){
+      console.log("is this working?");
+      console.log(this.customerForm.get('password').value == this.customerForm.get('confirmed_password').value)
+      return this.customerForm.get('password').value == this.customerForm.get('confirmed_password').value;
+    }
   }
 }
