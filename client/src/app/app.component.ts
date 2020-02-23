e3f80fb94a753b7b05a4d1f292c5b7540cd35f36
Nha Vuong <46470817+nhavuong@users.noreply.github.com>
2020-02-23 03:07:29 -0800

    nhatv-navbar scroll effect

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { AuthenticationService } from './authentication.service';
 import { faFacebook,faYoutube, faYelp} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faPhone, faMapMarkedAlt, faHandHoldingUsd, faDoorOpen, faLaughWink} from '@fortawesome/free-solid-svg-icons';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trigger, state, transition, style, animate } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
+
+declare const window: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[ 
+    trigger('fade',
+    [ 
+      state('void', style({ opacity : 0})),
+      transition(':enter',[ animate(300)]),
+      transition(':leave',[ animate(500)]),
+    ]
+)]
 
 })
 export class AppComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
 
-  constructor(public auth: AuthenticationService, private fb: FormBuilder) { }
+  constructor(public auth: AuthenticationService, private fb: FormBuilder, @Inject(DOCUMENT) document) { }
 
   faFacebook = faFacebook;
   faYoutube = faYoutube;
@@ -46,4 +58,15 @@ export class AppComponent implements OnInit {
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
 }
 
+  // Change color on scroll
+  header_variable = false;
+  @HostListener('document:scroll', ['$event'])
+  scrollfunction(){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+      this.header_variable = true;
+    }
+    else {
+      this.header_variable = false;
+    }
+  }
 }
