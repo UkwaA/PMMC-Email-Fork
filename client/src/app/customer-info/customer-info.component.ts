bae5db37604bc3c5e1dc104dbf8598e68ed2391f
UkwaA <uakkum@uci.edu>
2020-03-05 15:59:54 -0800

    -uakkum added confirmation box

--- client/src/app/customer-info/customer-info.component.ts
+++ client/src/app/customer-info/customer-info.component.ts
@@ -4,6 +4,8 @@ import { AuthenticationService } from '../authentication.service'
 import { Router, ActivatedRoute } from '@angular/router'
 import { CustomerData } from '../data/customer-data'
 import { CustomerService } from '../services/customer.services'
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ModalDialogComponent } from '../components/modal-dialog/modal-dialog.component';
 
 
 @Component({
@@ -30,7 +32,8 @@ export class CustomerRegisterComponent implements OnInit {
     }
   
     constructor(private auth: AuthenticationService, private router: Router, private route:ActivatedRoute,
-      private formBuilder: FormBuilder, public customerService:CustomerService) { }
+      private formBuilder: FormBuilder, public customerService:CustomerService,
+      public matDialog: MatDialog) { }
 
   ngOnInit() {
     this.customerInfoForm = this.formBuilder.group({
@@ -59,6 +62,42 @@ export class CustomerRegisterComponent implements OnInit {
   }
 
   get f() { return this.customerInfoForm.controls; }
+
+  openModal(){
+    //Validate form before open modal dialog
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.customerInfoForm.invalid) {
+        return;
+    }
+
+    //Configure Modal Dialog
+    const dialogConfig = new MatDialogConfig();
+    // The user can't close the dialog by clicking outside its body
+    dialogConfig.disableClose =true;
+    dialogConfig.id = "modal-component";
+    dialogConfig.height = "auto";
+    dialogConfig.maxHeight = "500px";
+    dialogConfig.width = "350px";
+    dialogConfig.data = {
+        title: "Register Confirmation",
+        description: "All information is correct?",            
+        actionButtonText: "Confirm",   
+        numberOfButton: "2"         
+      }
+      // https://material.angular.io/components/dialog/overview
+    // https://material.angular.io/components/dialog/overview
+    const modalDialog = this.matDialog.open(ModalDialogComponent, dialogConfig);
+    modalDialog.afterClosed().subscribe(result =>{
+        if(result == "Yes"){
+            //call register function                
+            this.finishRegister()
+        }
+        else{
+            console.log("stop")                
+        }
+    })
+}
 
   passwordsMatch(): any{
     console.log("Checking passowrds");
