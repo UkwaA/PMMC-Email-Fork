f7ffe1659ed5388bce3e13f134e00d7763a8734c
nhathuy <tonyhuy007@gmail.com>
2020-03-13 14:09:21 -0700

    hnluong1-server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const serveStatic = require('serve-static')
 const port = process.env.PORT || 3000
+const path = require('path');
 
-app.use(express.static('public'));
 app.use(bodyParser.json())
 app.use(cors())
 app.use(
@@ -26,11 +26,14 @@ app.use('/service', Email)
 var Customer = require('./routes/Customers')
 app.use('/customers', Customer)
 
+// Folder for photo
+app.use(express.static('public'));
+
 /************** Use for deploy on AWS **************/
-// app.use(express.static(__dirname + '/dist/client'));
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/client'))
-// });
+app.use(express.static(__dirname + '/dist/client'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/client'))
+});
 /**************************************************/
 
 app.listen(port , () =>{
