400993c42f4d79702f099d3d8ea29b300256d92c
anhnn5 <anhnn5@uci.edu>
2020-04-03 15:18:03 -0700

    anhnn5 - add AuthRoleGuard for Dashboard

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -90,7 +90,7 @@ const routes : Routes = [
     component: ProfileComponent,
     canActivate: [AuthGuardService], 
     children: [
-      {path: '', component: DashboardComponent},
+      {path: '', component: DashboardComponent, canActivate: [AuthRoleGuardService]},
       {path: 'createprogram', component: CreateProgramComponent, canActivate: [AuthRoleGuardService]},
       {path: 'program-management', component: ProgramManagementComponent, canActivate: [AuthRoleGuardService]},
       {path: 'view-schedule', component: ViewScheduleComponent, canActivate: [AuthRoleGuardService]},
