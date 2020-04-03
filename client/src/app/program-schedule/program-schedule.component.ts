827b9223ba18bdc611bcd047231ea260f6b8830f
anhnn5 <anhnn5@uci.edu>
2020-04-03 11:03:19 -0700

    anhnn5 - add calendar to program-schedule page

--- client/src/app/program-schedule/program-schedule.component.ts
+++ client/src/app/program-schedule/program-schedule.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProgramServices } from '../services/program.services';
 import { ProgramData } from '../data/program-data';
+import { ProgramScheduleService } from '../services/schedule.services';
+import { SchedulerEvent } from '@progress/kendo-angular-scheduler';
 
 @Component({
     templateUrl: './program-schedule.component.html',
@@ -12,9 +14,13 @@ export class ProgramScheduleComponent implements OnInit{
     ProgramPK: number;
     ProgramType: number;
     programDetails: ProgramData;
+    public selectedDate: Date = new Date();
+    public allEvents: SchedulerEvent[]
+    allSchedules: any = []
     
     constructor(private route: ActivatedRoute,
-        private service: ProgramServices){
+        private service: ProgramServices,
+        private programScheduleServices: ProgramScheduleService){
 
     }
     ngOnInit(){
@@ -25,9 +31,22 @@ export class ProgramScheduleComponent implements OnInit{
         this.service.getProgramHeaderDeatailsByID(this.ProgramPK).subscribe(details => {
             this.programDetails = details;
             this.ProgramType = details.ProgramType;
-            console.log(this.programDetails);
             document.getElementById("program_name").innerHTML = this.programDetails.Name;
             document.getElementById("program_desc").innerHTML = this.programDetails.Description;
       })
+
+        this.programScheduleServices.getScheduleById(this.ProgramPK).subscribe(schedules =>{
+            schedules.forEach(element =>{
+                    var event = {
+                        id : element.SchedulePK,
+                        title : this.programDetails.Name,                            
+                        start : new Date(element.Date + 'T' + element.StartTime),
+                        end : new Date(element.Date + 'T' + element.EndTime)                        
+                    }
+                    this.allSchedules.push(event)
+                }                    
+            )                
+        })        
+        this.allEvents = this.allSchedules
     }
 }
