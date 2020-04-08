2c6427c862270b686e87cead5eb6eb8a83873057
vuhoang1608 <vuhoang1608@yahoo.com>
2020-04-08 11:39:11 -0700

    hoangt5 - FIx Guard Service to allow customer to see the dashboard

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -94,7 +94,7 @@ const routes : Routes = [
     component: ProfileComponent,
     canActivate: [AuthGuardService], 
     children: [
-      {path: '', component: DashboardComponent, canActivate: [AuthRoleGuardService]},
+      {path: '', component: DashboardComponent, canActivate: [AuthGuardService]},
       {path: 'createprogram', component: CreateProgramComponent, canActivate: [AuthRoleGuardService]},
       {path: 'program-management', component: ProgramManagementComponent, canActivate: [AuthRoleGuardService]},
       {path: 'view-schedule', component: ViewScheduleComponent, canActivate: [AuthRoleGuardService]},
