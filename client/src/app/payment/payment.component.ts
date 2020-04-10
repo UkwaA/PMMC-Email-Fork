907bf5a9d437032b61e5c20caede9d681bea2f8c
anhnn5 <anhnn5@uci.edu>
2020-04-09 22:55:05 -0700

    anhnn5 - create formgroup and add basic validation for payment page

--- client/src/app/payment/payment.component.ts
+++ client/src/app/payment/payment.component.ts
@@ -1,5 +1,6 @@
 import { OnInit, Component } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './payment.component.html',
@@ -7,9 +8,51 @@ import { AuthenticationService } from '../authentication.service';
 })
 
 export class PaymentComponent implements OnInit{
-    constructor(public auth: AuthenticationService){}
+    paymentForm: FormGroup;
+    submitted = false;
+
+    constructor(public auth: AuthenticationService,
+        private fb: FormBuilder){}
 
     ngOnInit(){
 
+        this.paymentForm = this.fb.group({
+            FirstName: ["", [Validators.required]],
+            LastName: ["", [Validators.required]],
+            Address: ["", [Validators.required]],
+            State: ["", [Validators.required]],
+            Zipcode: ["", [Validators.required]],
+            NameOnCard: ["", [Validators.required]],
+            CardNumber: ["", [Validators.required]],
+            Expiration: ["", [Validators.required]],
+            CVV: ["", [Validators.required]],
+        })
+
+    }
+
+    get f() {
+        return this.paymentForm.controls;
+    }
+
+    // Clear data when click on input field
+    onFocus(event) {
+        if (event.target.value == 0) event.target.value = "";
+    }
+
+    // Restore data when lose focus on input field
+    lostFocus(event) {
+        if (event.target.value === 0 || event.target.value === "") {
+            event.target.value = 0;
+        }
+    }
+
+    payment(){
+        this.submitted = true;
+        if (this.paymentForm.invalid) {
+            console.log("invalid");
+            return;
+        }
+
+        console.log("valid");
     }
 }
