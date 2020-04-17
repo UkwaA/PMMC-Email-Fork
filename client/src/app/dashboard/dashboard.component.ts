3ef2345a049bfebd9c95a64c3a9f2cb126ea4a2b
anhnn5 <anhnn5@uci.edu>
2020-04-16 23:17:53 -0700

    anhnn5- add graph for admin's dashboard using ng2-charts

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -12,6 +12,38 @@ declare var $: any;
 export class DashboardComponent implements OnInit {
   role:string;
   searchText: string;
+  /* CHART USING NG2-CHARTS */
+  title = 'Bar Chart Example Using ng2-charts';
+
+  // ADD CHART OPTIONS. 
+  chartOptions = {
+    responsive: true    // THIS WILL MAKE THE CHART RESPONSIVE (VISIBLE IN ANY DEVICE).
+  }
+
+  labels =  ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+  // STATIC DATA FOR THE CHART IN JSON FORMAT.
+  chartData = [
+    {
+      label: '1st Year',
+      data: [21, 56, 4, 31, 45, 15, 57, 61, 9, 17, 24, 59] 
+    },
+    { 
+      label: '2nd Year',
+      data: [47, 9, 28, 54, 77, 51, 24]
+    }
+  ];
+
+  // CHART COLOR.
+  colors = [
+    { // 1st Year.
+      backgroundColor: 'rgba(77,83,96,0.2)'
+    },
+    { // 2nd Year.
+      backgroundColor: 'rgba(30, 169, 224, 0.8)'
+    }
+  ]
+   /* FINISH CHART USING NG2-CHARTS */
   
 
   // Dropdown Menu Option
@@ -39,6 +71,11 @@ export class DashboardComponent implements OnInit {
   }
   clearSearch() {
     this.searchText = "";
+  }
+
+  //On Click event of chart
+  onChartClick(event) {
+    console.log(event);
   }
 
   // Catch the event dropdown menu (later)
