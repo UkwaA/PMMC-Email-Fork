40375cd6531eb74bae2ad7ec265ba3449d9ce94b
nhathuy <tonyhuy007@gmail.com>
2020-04-17 12:21:12 -0700

    hnluong1-update Express

--- routes/Reservation.js
+++ routes/Reservation.js
@@ -8,7 +8,6 @@ const ReservationHeader = require('../models/ReservationHeader')
 const ReservationGroupDetails = require('../models/ReservationGroupDetails')
 const ReservationIndividualDetails= require('../models/ReservationIndividualDetails')
 
-
 reservation.use(bodyParser.json());
 reservation.use(cors())
 
@@ -17,5 +16,8 @@ reservation.use(cors())
 *  Add new ReservationHeader data         *
 *******************************************/
 reservation.post('/add-new-reservation', (req, res) => {
-    
+
 })
+
+// ========================END=====================
+module.exports = reservation;
