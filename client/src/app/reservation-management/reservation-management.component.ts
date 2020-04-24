ce11b4f4184d0d4c622205f8bb21f4c2b871c268
anhnn5 <anhnn5@uci.edu>
2020-04-23 23:45:59 -0700

    anhnn5 - add mockup to the reservation management page

--- client/src/app/reservation-management/reservation-management.component.ts
+++ client/src/app/reservation-management/reservation-management.component.ts
@@ -1,14 +1,27 @@
-import { OnInit, Component } from '@angular/core';
+import { OnInit, Component, ViewEncapsulation } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component ({
     templateUrl: '/reservation-management.component.html',
-    styleUrls: ['./reservation-management.component.css']
+    styleUrls: ['./reservation-management.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 
 export class ReservationManagement implements OnInit{
-    constructor(){}
+    role:string;
+
+    constructor(private auth: AuthenticationService){}
     
     ngOnInit(){
+        this.auth.profile().subscribe(
+            user => {
+                this.role = user.Role_FK;
+                console.log(this.role)
+            },
+            err => {
+                console.error(err)
+            }
+          )
 
     }
 }
